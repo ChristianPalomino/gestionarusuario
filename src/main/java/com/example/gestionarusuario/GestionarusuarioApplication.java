00119f91b6bdec61d20cf228a6c8a41f0b21ad89
christianpalomino <christianspq@gmail.com>
2023-04-25 10:44:21 -0500

    First commit

---
+++ src/main/java/com/example/gestionarusuario/GestionarusuarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionarusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionarusuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionarusuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionarusuario/GestionarusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionarusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionarusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
